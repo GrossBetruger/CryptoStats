581e364c7ab897dd265e9a882e8d7de4d79f1e2c
Oren Koriat <oren@cynerio.co>
2021-01-14 12:27:48 +0200

    add eth stats

--- import_data_cmd.sh
+++ import_data_cmd.sh
@@ -2,4 +2,5 @@
 
 set -e 
 
-sqlite3 --cmd ".mode csv" --cmd ".import BTC-USD_daily_all_times.csv yahoo_btc_usd_daily" --cmd ".save crypto_stats.db"
+sqlite3 --cmd ".mode csv" --cmd ".import BTC-USD_daily_all_times.csv yahoo_btc_usd_daily" --cmd ".import ETH-USD_daily_all_times.csv yahoo_eth_usd_daily" --cmd ".save crypto_stats.db"
+
